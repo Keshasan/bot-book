79c38e271e51a5cbdd04cf510e50655f2f35d348
keshasan <kesha3084@gmail.com>
2021-11-17 15:11:50 +0200

    fixed keyError + refactor + b-day functionality finished

--- classes.py
+++ classes.py
@@ -7,14 +7,17 @@ from datetime import date
 class AdressBook(UserDict):
     @input_error
     def add_record(self, record):
+        """
+            Add new record to AdressBook as 'Record' object
+        """
         self.data[record.name.value] = record
 
     def show_all_records(self):
+        """
+            Print all records in AdressBook
+        """
         for name, phones in self.data.items():
             print(f'{name}: {self.data[name].get_phones()}')
-
-    # def show_birthday(self, name):
-    #     print(self.data[name].get_birthday())
 
 
 class Record:
@@ -24,15 +27,24 @@ class Record:
         self.birthday = None
 
     def add_phone(self, phone):
+        """
+            Add 'Phone' object to phones list.
+        """
         self.phones.append(Phone(phone))
 
     @input_error
     def add_birthday(self, birthday_date):
+        """
+            Add 'Birthday' object
+        """
         birthday_date = date.fromisoformat(birthday_date)
         self.birthday = Birthday(birthday_date)
 
     @input_error
     def delete_phone(self, candidate_phone):
+        """
+            Delete "Phone" object from phones list.
+        """
         candidate_phone = Phone(candidate_phone)
         for phone in self.phones:
             if phone.value == candidate_phone.value:
@@ -40,19 +52,26 @@ class Record:
 
     @input_error
     def change_phone(self, old_phone, new_phone):
-
+        """
+            Delete "Phone" object from phones list.
+        """
         if old_phone in self.get_phones():
             self.delete_phone(old_phone)
             self.add_phone(new_phone)
         else:
             self.add_phone(new_phone)
 
-    @input_error
     def get_phones(self):
+        """
+            Returns list phones in Record
+        """
         return [phone.value for phone in self.phones]
 
     @input_error
     def get_birthday(self):
+        """
+            Return record birthday if it is added
+        """
         if self.birthday is not None:
             return self.birthday.value
         else:
@@ -60,6 +79,11 @@ class Record:
 
     @input_error
     def days_to_birthday(self):
+        """
+            If b-day is added: counts days before next one.
+
+        """
+
         if self.birthday is None:
             print(f'No b-day added for {self.name.value}')
             return None
@@ -74,7 +98,6 @@ class Record:
         print(f"{days_delta} days before {self.name.value}'s Birthday.")
         print(f"Birthday date is: {self.birthday.value.strftime('%d %b %Y')}")
         return days_delta
-
 
 
 class Field:
@@ -135,8 +158,7 @@ def main():
             if len(commands) != 3:
                 print('Incorrect command usage.')
             else:
-                name = commands[1]
-                phone_number = commands[2]
+                name, phone_number = commands[1], commands[2]
                 if user_book.data.get(name):
                     user_book.data[name].add_phone(phone_number)
                 else:
@@ -147,9 +169,7 @@ def main():
             if len(commands) != 4:
                 print('Incorrect command usage.')
             else:
-                name = commands[1]
-                old_phone_number = commands[2]
-                new_phone_number = commands[3]
+                name, old_phone_number, new_phone_number = commands[1], commands[2], commands[3]
                 user_book[name].change_phone(
                     old_phone_number, new_phone_number)
 
@@ -165,12 +185,19 @@ def main():
         elif command == 'bday':
             if len(commands) == 2:
                 name = commands[1]
-                user_book.data[name].days_to_birthday()
+                if user_book.data.get(name):
+                    user_book.data[name].days_to_birthday()
+
+                else:
+                    print("Unknown username")
 
             elif len(commands) == 3:
-                name = commands[1]
-                date_ = commands[2]
-                user_book[name].add_birthday(date_)
+                name, birthday_date = commands[1], commands[2]
+
+                if user_book.data.get(name):
+                    user_book[name].add_birthday(birthday_date)
+                else:
+                    print("Unknown username")
 
             else:
                 print('Incorrect command usage!')
