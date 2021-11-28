b6e34a0bfeb083475b847d95fbe82d4dfd10ecd7
Keshasan <kesha3084@gmail.com>
2021-11-28 08:49:31 +0200

    save book add

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import sys
-
+import os
+import pickle
 from AdressBook import AdressBook
 
 
 def main():
     print('Hello, this is bot assistant. Type "hello" or "help" to see list of commands \n')
     command = None
-    user_book = AdressBook()
+    if os.path.exists('data.bin'):
+        user_book = load_book()
+    else:
+        user_book = AdressBook()
     # page iterator for command next - to show next records
     page_iterator = None
     while command not in ('exit', 'quit', 'good'):
@@ -21,8 +25,10 @@ def main():
                 'Type "change username +380********* +380*********" to change phone number in your phone book.')
             print('Type "show all" to get all your contacts.')
             print('Type "phone Username" to find person phone number by name.')
-            print('Type "bday Username" to find username birthday and count days before birthday.')
-            print('Type "bday Username YYYY-MM-DD" to set username birthday in ISO format.')
+            print(
+                'Type "bday Username" to find username birthday and count days before birthday.')
+            print(
+                'Type "bday Username YYYY-MM-DD" to set username birthday in ISO format.')
             print('Types "pages 3" to define pagination for 3 records in book')
             print('Types "pages next" to show next 3 records')
             print('Type "exit" or "quit" or "good bye" to finish my work.')
@@ -100,7 +106,18 @@ def main():
         else:
             print("Incorrect command. I don't understand you =(")
 
+    save_book(user_book)
     sys.exit()
+
+
+def load_book():
+    with open('data.bin', 'rb') as file:
+        return pickle.load(file)
+
+
+def save_book(book):
+    with open('data.bin', 'wb') as file:
+        pickle.dump(book, file)
 
 
 if __name__ == '__main__':
